3849d2898dc8b630c019144056b1bd06cf504f18
ericremoroza <eric.remoroza@outlook.com>
2019-01-10 19:12:57 -0500

    Updates

---
+++ precache-manifest.b716e9eed12c14336f00b5f50646e6c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "738183ac2e54cba1d794",
+    "url": "/clicky-game/static/js/main.738183ac.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/clicky-game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "738183ac2e54cba1d794",
+    "url": "/clicky-game/static/css/main.da07bd09.chunk.css"
+  },
+  {
+    "revision": "2d1106b1e591bcbf8d10bcee37d289b2",
+    "url": "/clicky-game/index.html"
+  }
+];
